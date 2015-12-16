317c9b5419a724115ab07635e6bb5675d9454b78
薛丞宏 <ihcaoe@gmail.com>
2015-12-16 15:51:44 +0800

    除了華語以外，賰的語言的辭典檔攏好矣

--- bizu/切錄音檔.py
+++ bizu/切錄音檔.py
@@ -1,5 +1,14 @@
+from itertools import chain
+
 import xlrd
+
+
 from bizu.參數 import xls所在
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from 臺灣言語工具.解析整理.轉物件音家私 import 轉物件音家私
+from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
+from 臺灣言語工具.解析整理.字物件篩仔 import 字物件篩仔
+from 臺灣言語工具.音標系統.閩南語.教會羅馬字音標 import 教會羅馬字音標
 
 
 class 切錄音檔:
@@ -28,3 +37,33 @@ class 切錄音檔:
             for 詞 in 這筆資料:
                 詞資料.append([詞])
         return 句資料, 詞資料
+
+    @classmethod
+    def _轉標仔佮辭典(cls, 語句格式):
+        標仔資料, 辭典資料 = [], set()
+        for 語句資料 in 語句格式:
+            語句陣列 = []
+            辨識單位 = []
+            for 語言, 語句 in 語句資料:
+                語句陣列.append(語句.replace(' ', '_'))
+                if 語言 == '語詞編號':
+                    辨識單位.append(語句.replace('-', ''))
+                elif 語言 == '臺語':
+                    句物件=拆文分析器.產生對齊句(語句,語句)
+                    預設音物件=轉物件音家私.轉音(教會羅馬字音標, 句物件)
+                    音值物件=轉物件音家私.轉音(臺灣閩南語羅馬字拼音, 預設音物件,函式=                                    '音值')
+                    音值陣列=[]
+                    for 字物件 in 字物件篩仔.篩出字物件(音值物件):
+                        try:
+                            音值陣列.extend(字物件.音[:2])
+                        except:
+                            pass
+                    辨識單位.append(音值陣列)
+                else:
+                    辨識單位.append(語句)
+            標仔='，'.join(語句陣列)
+            標仔資料.append(標仔)
+            辭典資料.add(
+                '{} {}'.format(標仔, ' '.join(chain.from_iterable(辨識單位)))
+            )
+        return 標仔資料, 辭典資料
--- bizu/試驗/test切錄音檔試驗.py
+++ bizu/試驗/test切錄音檔試驗.py
@@ -26,3 +26,68 @@ class 切錄音檔試驗(TestCase):
         self.assertIn([
             ('Kaxabu', 'punu')
         ], 詞資料)
+
+    def test_檢查標仔資料(self):
+        標仔資料, _辭典資料 = 切錄音檔._轉標仔佮辭典([
+            [
+                ('語詞編號', '01A-001'), ('臺語', 'thâu ê tsoân-pō•'),
+                ('華語', '頭（整個）'), ('Kaxabu', 'punu')
+            ]
+        ])
+        self.assertEqual(
+            標仔資料,
+            ['01A-001，thâu_ê_tsoân-pō•，頭（整個），punu']
+        )
+
+    def test_檢查辭典語詞編號(self):
+        _標仔資料, 辭典資料 = 切錄音檔._轉標仔佮辭典([
+            [('語詞編號', '01A-001')]
+        ])
+        self.assertEqual(
+            辭典資料,
+            set(['01A-001 0 1 A 0 0 1'])
+        )
+
+    def test_檢查辭典臺語(self):
+        _標仔資料, 辭典資料 = 切錄音檔._轉標仔佮辭典([
+            [('臺語', 'thâu ê tsoân-pō•')]
+        ])
+        self.assertEqual(
+            辭典資料,
+            set(['thâu_ê_tsoân-pō• tʰ au ʔ e ts uan p o'])
+        )
+
+    def test_檢查辭典Kaxabu(self):
+        _標仔資料, 辭典資料 = 切錄音檔._轉標仔佮辭典([
+            [('Kaxabu', 'punu')]
+        ])
+        self.assertEqual(
+            辭典資料,
+            set(['punu p u n u'])
+        )
+
+    def test_檢查辭典資料(self):
+        _標仔資料, 辭典資料 = 切錄音檔._轉標仔佮辭典([
+            [
+                ('語詞編號', '01A-001'), ('臺語', 'thâu ê tsoân-pō•'),
+                ('華語', '頭（整個）'), ('Kaxabu', 'punu')
+            ]
+        ])
+        self.assertEqual(
+            辭典資料,
+            set([
+                '01A-001，thâu_ê_tsoân-pō•，頭（整個），punu 0 1 A 0 0 1 tʰ au ʔ e ts uan p o ㄊ ㄡ p u n u'])
+        )
+
+    def test_仝款資料辭典出現一擺(self):
+        _標仔資料, 辭典資料 = 切錄音檔._轉標仔佮辭典([
+            [
+                ('語詞編號', '01A-001'), ('臺語', 'thâu ê tsoân-pō•'),
+                ('華語', '頭（整個）'), ('Kaxabu', 'punu')
+            ],
+            [
+                ('語詞編號', '01A-001'), ('臺語', 'thâu ê tsoân-pō•'),
+                ('華語', '頭（整個）'), ('Kaxabu', 'punu')
+            ]
+        ])
+        self.assertEqual(len(辭典資料), 1)
