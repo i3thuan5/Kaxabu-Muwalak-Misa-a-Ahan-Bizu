8abef29f1b7e366e8339b3290ec503a29b2f2d31
薛丞宏 <ihcaoe@gmail.com>
2016-01-29 14:12:30 +0800

    前端詞條處理好矣

--- 前端/src/元素/詞條/詞條標題.jsx
+++ 前端/src/元素/詞條/詞條標題.jsx
@@ -5,9 +5,9 @@ import {Link} from 'react-router';
 import superagent from 'superagent-bluebird-promise';
 import Debug from 'debug';
 
-var debug = Debug('kaxabu:詞條');
+var debug = Debug('kaxabu:詞條標題');
 
-class 詞條 extends React.Component {
+class 詞條標題 extends React.Component {
 
   componentWillMount () { this.props.setQueryParams(this.props); }
 
@@ -17,23 +17,21 @@ class 詞條 extends React.Component {
   }
 
   render () {
-    let {資料} = this.props
-
     return (
         <tr>
-            <td>{資料.語詞編號}</td>
-            <td>{資料.噶哈巫語教材標記法}</td>
-            <td>{資料.噶哈巫語潘永歷標記法}</td>
-            <td>{資料.中文譯解}</td>
-            <td>{資料.臺語譯解}</td>
-            <td>{資料.備註}</td>
-            <td>{資料.出處}</td>
+            <th>語詞編號</th>
+            <th>噶哈巫語教材標記法</th>
+            <th>噶哈巫語潘永歷標記法</th>
+            <th>中文譯解</th>
+            <th>臺語譯解</th>
+            <th>備註</th>
+            <th>出處</th>
         </tr>
       );
   }
 }
 
-export default Transmit.createContainer(詞條, {
+export default Transmit.createContainer(詞條標題, {
   queries: {
   },
 });
