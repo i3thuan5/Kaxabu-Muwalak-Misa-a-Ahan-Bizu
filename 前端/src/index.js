a84058d03fe1ee8b7e52125f1a096b66393b9d63
薛丞宏 <ihcaoe@gmail.com>
2016-01-29 14:52:43 +0800

    加佈署的設定

--- 前端/src/index.js
+++ 前端/src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import Router, {Route, IndexRoute} from 'react-router';
 import 網站 from './網站/網站';
-import 查 from './頁/查/查';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 import './app.css';
 
