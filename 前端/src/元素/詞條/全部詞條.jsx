a76c40db10e97450ca03d5d53c4cef42827558e8
薛丞宏 <ihcaoe@gmail.com>
2017-02-09 10:07:10 +0800

    一擺顯示200條，較袂一開始攏卡牢

--- 前端/src/元素/詞條/全部詞條.jsx
+++ 前端/src/元素/詞條/全部詞條.jsx
@@ -10,16 +10,44 @@ import 詞條標題 from '../../元素/詞條/詞條標題';
 var debug = Debug('kaxabu:全部詞條');
 
 class 全部詞條 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      全開: this.預設顯示幾个(),
+    };
+  }
+
+  預設顯示幾个() {
+    return 200;
+  }
+
+  看閣較濟全開() {
+    let { 全開 } = this.state;
+    全開 += this.預設顯示幾个();
+    this.setState({ 全開 });
+  }
 
   componentWillMount () { this.props.setQueryParams(this.props); }
 
   componentWillReceiveProps (nextProps) {
     if (nextProps.關鍵字 === this.props.關鍵字) return;
     this.props.setQueryParams(nextProps);
+    this.setState({ 全開: this.預設顯示幾个() });
+  }
+
+  看閣較濟() {
+    if (this.props.辭典資料.符合資料.length > this.state.全開)
+    return (
+          <button onClick={this.看閣較濟全開.bind(this)}
+            className='ui icon button'>
+            再顯示多一點
+          </button>
+      );
   }
 
   render () {
-    let 詞條陣列 = this.props.辭典資料.符合資料.map((資料)=>(
+    let { 全開 } = this.state;
+    let 詞條陣列 = this.props.辭典資料.符合資料.slice(0, 全開).map((資料)=>(
         <詞條 key={資料.語詞編號} 資料={資料} 換音檔={this.props.換音檔}/>)
       );
     return (
@@ -30,6 +58,7 @@ class 全部詞條 extends React.Component {
                   {詞條陣列}
                 </tbody>
             </table>
+            {this.看閣較濟()}
         </div>
           );
   }
@@ -39,13 +68,13 @@ export default Transmit.createContainer(全部詞條, {
   queries: {
     辭典資料({ 後端網址, 關鍵字 }) {
       if (後端網址 === undefined)
-        return Promise.resolve({'符合資料':[{'語詞編號':'載入中……'}]});
+        return Promise.resolve({ '符合資料':[{ '語詞編號':'載入中……' }] });
       return superagent.get(後端網址 + '查')
           .query({ 關鍵字 })
           .then(({ body }) => (
             body
           ))
-          .catch((err) => ({'符合資料':[]}));
+          .catch((err) => ({ '符合資料':[] }));
     },
   },
 });
