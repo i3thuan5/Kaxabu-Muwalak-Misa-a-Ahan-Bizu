e179a950be3525daf60a590b0e740d7bffe56897
a8568730 <a8568730@hotmail.com>
2020-09-28 13:48:30 +0800

    * Sû-tiâu tiám tī <td>, tiō bián lāi-té chhàng <a> suah tín-tāng tio̍h bāng-chí.

--- 前端/src/元素/詞條/詞條.jsx
+++ 前端/src/元素/詞條/詞條.jsx
@@ -3,18 +3,15 @@ import React from 'react';
 const SuTiau = ({ 換音檔, 資料 }) => (
     <tr>
         <td>{資料.語詞編號}</td>
-        <td className="selectable">
-          <a href='#!' onClick={換音檔.bind(this, 資料.語詞編號, 'Kaxabu', 資料.噶哈巫語教材標記法)}
-          >{資料.噶哈巫語教材標記法}</a></td>
-        <td className="selectable">
-          <a href='#!' onClick={換音檔.bind(this, 資料.語詞編號, 'Kaxabu', 資料.噶哈巫語潘永歷標記法)}
-          >{資料.噶哈巫語潘永歷標記法}</a></td>
-        <td className="selectable">
-          <a href='#!' onClick={換音檔.bind(this, 資料.語詞編號, '華語', 資料.中文譯解)}
-          >{資料.中文譯解}</a></td>
-        <td className="selectable">
-          <a href='#!' onClick={換音檔.bind(this, 資料.語詞編號, '臺語', 資料.臺語譯解)}
-          >{資料.臺語譯解}</a></td>
+        <td className="selectable"
+          onClick={換音檔.bind(this, 資料.語詞編號, 'Kaxabu', 資料.噶哈巫語教材標記法)}
+          >{資料.噶哈巫語教材標記法}</td>
+        <td className="selectable" onClick={換音檔.bind(this, 資料.語詞編號, 'Kaxabu', 資料.噶哈巫語潘永歷標記法)}
+          >{資料.噶哈巫語潘永歷標記法}</td>
+        <td className="selectable" onClick={換音檔.bind(this, 資料.語詞編號, '華語', 資料.中文譯解)}
+          >{資料.中文譯解}</td>
+        <td className="selectable" onClick={換音檔.bind(this, 資料.語詞編號, '臺語', 資料.臺語譯解)}
+          >{資料.臺語譯解}</td>
         <td>{資料.備註}</td>
         <td>{資料.出處}</td>
     </tr>
