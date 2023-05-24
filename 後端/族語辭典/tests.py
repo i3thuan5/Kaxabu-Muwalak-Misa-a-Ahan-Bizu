1c1a5c225b737bbae0753ff0b4514a4431b5fc94
Tira <hong@ithuan.tw>
2023-05-24 17:25:42 +0800

    補試驗。

---
+++ 後端/族語辭典/tests.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.core.management import call_command
+from urllib.parse import urlencode
+
+class TshiThiann(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.setUpClass()
+        call_command('hue_xls')
+        call_command('hue_imtong')
+
+    def test_聽音檔(self):
+        self.client.get('聽?{}'.format(
+            urlencode({
+                '語詞編號': '01H-005',
+                '內容': '噶哈巫',
+            })
+        ))
+    def test_顯示全部資料(self):
+        顯示全部資料
+        self.client.get('聽?{}'.format(
+            urlencode({
+                '語詞編號': '01H-005',
+                '內容': '噶哈巫',
+            })
+        ))
+    def test_查(self):
+        查
+        self.client.get('聽?{}'.format(
+            urlencode({
+                '語詞編號': '01H-005',
+                '內容': '噶哈巫',
+            })
+        ))
